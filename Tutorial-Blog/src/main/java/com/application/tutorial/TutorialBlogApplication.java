9c26a99309932ae18a8caf3379d1b7a0fb91b487
PGaur <PGaur@PGaur-Laptop>
2017-09-19 09:37:32 +0530

    Starting Development for Tutorial Blog

---
+++ Tutorial-Blog/src/main/java/com/application/tutorial/TutorialBlogApplication.java
@@ -0,0 +1,12 @@
+package com.application.tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialBlogApplication.class, args);
+	}
+}
