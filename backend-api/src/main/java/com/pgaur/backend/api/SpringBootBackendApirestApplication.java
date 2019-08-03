8fbb5b80c36f878420ba325ec6a066b61c89bf01
pwngaur <pawangaur001@gmail.com>
2019-08-03 11:54:14 +0530

    commit rest api files

---
+++ backend-api/src/main/java/com/pgaur/backend/api/SpringBootBackendApirestApplication.java
@@ -0,0 +1,13 @@
+package com.pgaur.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendApirestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBackendApirestApplication.class, args);
+    }
+
+}
---
+++ backend-api/src/main/java/com/pgaur/backend/api/model/Customer.java
@@ -0,0 +1,73 @@
+package com.pgaur.backend.api.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.DATE)
+    private Date createAt;
+
+    @PrePersist
+    public void prePersist(){
+        createAt = new Date();
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreateAt() {
+        return createAt;
+    }
+
+    public void setCreateAt(Date createAt) {
+        this.createAt = createAt;
+    }
+}
